337dc89f2497a9dbb3970f92fe1d3e9276bee297
Carlos Marinho <carlos.marinho@pravaler.com.br>
2021-06-24 16:50:06 -0300

    first commit

---
+++ marketing/config/webpack.commom.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
