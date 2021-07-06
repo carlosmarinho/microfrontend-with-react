10b58635059050032d7aca9ab80d16ee97548697
Carlos Marinho <carlos.marinho@pravaler.com.br>
2021-07-06 12:05:35 -0300

    configurações e finalização de dev do container e marketing mais configuração de produção

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,32 +1,31 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
-const commonConfig = require('./webpack.commom');
+const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8081,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html',
     },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'marketing',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './MarketingApp': '.src/bootstrap'
-            },
-            // shared: ['react', 'react-dom'],
-            shared: packageJson.dependencies,
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
-}
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        // shared: ['react', 'react-dom'],           
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
